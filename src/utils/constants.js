9c7a6c78386ef48f42ba939417da5d964eed7028
Agung Maulana <114566958+agung7703@users.noreply.github.com>
2024-02-28 17:38:11 +0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const API_URL = "http://localhost:3004/";
-export const API_URL = "https://agung7703.github.io/kobara_data/db.json/";
+export const API_URL = "http://localhost:3004/";
+// export const API_URL = "https://agung7703.github.io/kobara_data/db.json/";
